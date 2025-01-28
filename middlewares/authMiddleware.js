187b7b1aeea26478e1bade0666009d6befccc743
rex atuzie <rexatuzie@gmail.com>
2025-01-28 17:22:38 +0100

    ----

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,37 +3,31 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 const protect = asyncHandler(async (req, res, next) => {
-  try {
-    // Extract the token from cookies
-    const token = req.cookies?.token;
+  // Extract the token from cookies
+  const token = req.cookies?.token;
 
-    // Check if the token exists
-    if (!token) {
-      res.status(401);
-      throw new Error("Not authorized, please login");
-    }
-
-    // Verify the token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Fetch the user from the database
-    const user = await User.findById(decoded.userId).select("-password");
-
-    if (!user) {
-      res.status(404);
-      throw new Error("User not found");
-    }
-
-    // Attach the user to the request object
-    req.user = user;
-
-    // Proceed to the next middleware
-    next();
-  } catch (error) {
-    // Handle JWT verification errors
+  // Check if the token exists
+  if (!token) {
     res.status(401);
-    throw new Error("Not authorized, invalid token");
+    throw new Error("Not authorized, please login");
   }
+
+  // Verify the token
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  // Fetch the user from the database
+  const user = await User.findById(decoded.userId).select("-password");
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // Attach the user to the request object
+  req.user = user;
+
+  // Proceed to the next middleware
+  next();
 });
 
 module.exports = { protect };
