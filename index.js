72e4db08688bf98b3c5a4d7f8a827d1cd6dd0225
rex atuzie <rexatuzie@gmail.com>
2025-01-29 17:14:53 +0100

    ---

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/election", electionRoutes);
 app.use("/api/v1/ballot", ballotRoutes);
